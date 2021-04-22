bd772b0aa1dc94ea4e67c16bcd22108bd8b27dc1
Rugved Tipare <73734376+ProProgrammer2504@users.noreply.github.com>
2021-04-22 14:28:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmeEE21oYSniUzVI6qz8OiXdHF_puPlio",
-  authDomain: "whatsapp-clone-9d7a8.firebaseapp.com",
-  projectId: "whatsapp-clone-9d7a8",
-  storageBucket: "whatsapp-clone-9d7a8.appspot.com",
-  messagingSenderId: "862332310190",
-  appId: "1:862332310190:web:9a76b37e634ffa7ee7133f",
+//  Your Firebase config goes here!
 };
 
 const app = !firebase.apps.length
